65fc5775ef815d4df00b9a14816a9a6606cefbde
updevil01 <upkungja@gmail.com>
2024-10-16 01:20:38 +0700

    add edit function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
 const [Todos,SetTodos] = useState([]);
 const [InputValue,SetInputValue] = useState('');
 
+
+const [EditInputValue,SetEditInputValue] = useState('');
+const [IsEditing,SetEditing] = useState(false);
+const [editIndex,SetEditIndex] = useState(-1);
+
 const Addtodos =()=>{
   if(InputValue.trim() !== ''){
     SetTodos([...Todos,InputValue.trim()]);
@@ -20,6 +25,21 @@ const DeleteTodos = (index)=>{
   SetTodos(deleteTodos);
 }
 
+const Edit =(index)=>{
+  SetEditIndex(index)
+  SetEditing(true)
+  SetEditInputValue(Todos[index])
+}
+
+const ConfirmEdit=()=>{
+  const updated = Todos.map((result,index)=>{
+    return index === editIndex ? EditInputValue : result
+  });
+SetTodos(updated);
+    SetEditInputValue(''); // รีเซ็ต EditInputValue
+    SetEditing(false);
+    SetEditIndex(-1); // รีเซ็ต index ที่แก้ไขเป็น -1
+}
   return (
     <div className="App">
       <h1>To-do List</h1>
@@ -28,12 +48,28 @@ const DeleteTodos = (index)=>{
       value={InputValue}
       onChange={(e)=>SetInputValue(e.target.value)}
       />
-      <button onClick={Addtodos}>Add</button>
+       <button onClick={Addtodos}>
+       ADD
+       </button>
       <ui>
       {Todos.map((result, index) => (
-  <li className="todoItem" key={index}>
-    <span className="todoText">{result}</span> {/* ข้อความ todo */}
-    <button className="removeButton" onClick={() => DeleteTodos(index)}>Remove</button>
+    <li className="todoItem" key={index}>
+    {IsEditing && editIndex === index ?(
+         <input
+         type='text'
+         value={EditInputValue}
+         onChange={(e)=>SetEditInputValue(e.target.value)}
+        />
+    ):(
+      <span className="todoText">{result}</span> 
+    )}
+      { IsEditing && editIndex === index ? (
+            <button onClick={()=>ConfirmEdit(index)}> Con</button>
+      ):(
+        <button onClick={()=>Edit(index)}> Edit</button>
+      )}
+        <button className="removeButton" onClick={() => DeleteTodos(index)}>Remove</button>
+    
   </li>
 ))}
       </ui>
